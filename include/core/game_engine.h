4f721492b88f7ffefba37c1cd144b7b093a673d3
felipe <felipe.z.assumpcao@gmail.com>
2021-04-17 21:53:21 -0500

    Create starting methods for game engine

--- include/core/game_engine.h
+++ include/core/game_engine.h
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <deque>
 #include "card.h"
 
 using std::string;
@@ -9,15 +10,26 @@ namespace blackjack{
     class GameEngine {
     public:
         GameEngine();
-        void Shuffle();
+        void StartDeal();
+        void Hit();
+        void Stand();
         
         
     private:
         vector<Card> dealer_cards_;
         vector<Card> player_cards_;
-        const vector<string> kDeck = {"As","1s","2s","3s","4s","5s","6s","7s","8s","9s","Ts","Js","Qs","Ks",
+        std::deque<string> deck_;
+        
+        vector<string> kDeckCards = {"As","1s","2s","3s","4s","5s","6s","7s","8s","9s","Ts","Js","Qs","Ks",
                                       "Ac","1c","2c","3c","4c","5c","6c","7c","8c","9c","Tc","Jc","Qc","Kc",
                                       "Ah","1h","2h","3h","4h","5h","6h","7h","8h","9h","Th","Jh","Qh","Kh",
                                       "Ad","1d","2d","3d","4d","5d","6d","7d","8d","9d","Td","Jd","Qd","Kd",};
+        
+        const size_t kBlackjackVaLue = 21;
+        const size_t kDealerStandValue = 17;
+
+        size_t CalculateTotalValue(const vector<Card> cards);
+        void Deal(bool to_dealer, bool face_up);
+
     };
 }
--- src/core/game_engine.cc
+++ src/core/game_engine.cc
@@ -1,10 +1,62 @@
 #include "../../include/core/game_engine.h"
+#include <random>
 
-void blackjack::GameEngine::Shuffle() {
-    std::random_shuffle(kDeck.begin(), kDeck.end());
+using namespace blackjack;
+GameEngine::GameEngine() {
+    
 }
 
-blackjack::GameEngine::GameEngine() {
-
+void blackjack::GameEngine::StartDeal() {
+    deck_.clear();
+    std::shuffle(kDeckCards.begin(), kDeckCards.end(), std::mt19937(std::random_device()()));
+    for (std::string card_info : kDeckCards) {
+        deck_.push_back(card_info);
+    }
+    player_cards_.clear();
+    dealer_cards_.clear();
+    player_cards_.push_back(Card(deck_.front(), true));
+    deck_.pop_front();
+    dealer_cards_.push_back(Card(deck_.front(), false));
+    deck_.pop_front();
+    player_cards_.push_back(Card(deck_.front(), true));
+    deck_.pop_front();
+    dealer_cards_.push_back(Card(deck_.front(), true));
+    deck_.pop_front();
+    
+    if (CalculateTotalValue(player_cards_) == kBlackjackVaLue) {
+        //TODO
+    } 
 }
+
+void GameEngine::Hit() {
+    player_cards_.push_back(Card(deck_.front(), true));
+    deck_.pop_front();}
+
+void GameEngine::Stand() {
+    dealer_cards_[1].TurnOver();
+    
+    while (CalculateTotalValue(dealer_cards_) < kDealerStandValue) {
+        de
+    }
+}
+
+size_t GameEngine::CalculateTotalValue(const vector<Card> cards) {
+    size_t total = 0;
+    for (Card card : cards) {
+        total += card.GetValue();
+    }
+    return total;
+}
+
+void GameEngine::Deal(bool to_dealer, bool face_up) {
+    if (to_dealer) {
+        dealer_cards_.push_back(Card(deck_.front(), face_up));
+        
+    } else {
+        player_cards_.push_back(Card(deck_.front(), face_up));
+    }
+    deck_.pop_front();
+}
+
+
 
