6ec6d5e03e3da8a663968aa728602d98dce829af
felipe <felipe.z.assumpcao@gmail.com>
2021-04-30 17:10:28 -0500

    New branch

--- src/core/game_engine.cc
+++ src/core/game_engine.cc
@@ -5,6 +5,7 @@
 namespace blackjack {
     
 GameEngine::GameEngine(bool deal_delay_on, size_t player_balance) {
+    //TODO add double down?
     deck_cards_ = {"As", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "Ts", "Js", "Qs", "Ks",
                    "Ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "Tc", "Jc", "Qc", "Kc",
                    "Ah", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "Th", "Jh", "Qh", "Kh",
